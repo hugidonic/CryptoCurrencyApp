a5cb396b7233a9bdf1dc205bea7924cc48807424
Hugidonic <netvaca@ya.ru>
2023-04-29 15:17:40 +0300

    Models

---
+++ domain/src/main/java/com/hugidonic/domain/entities/CoinDetailsModel.kt
@@ -0,0 +1,12 @@
+package com.hugidonic.domain.entities
+
+data class CoinDetailsModel(
+    val coinId: String,
+    val name: String,
+    val description: String,
+    val symbol: String,
+    val rank: Int,
+    val isActive: Boolean,
+    val tags: List<String>,
+    val team: List<TeamMemberModel>
+)
---
+++ domain/src/main/java/com/hugidonic/domain/entities/CoinModel.kt
@@ -0,0 +1,9 @@
+package com.hugidonic.domain.entities
+
+class CoinModel(
+    val id: String,
+    val isActive: Boolean,
+    val name: String,
+    val rank: Int,
+    val symbol: String,
+)
